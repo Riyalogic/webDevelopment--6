e6f2d7e834cb002f1e0eca9d223e6ad27c65f9ab
Riyalogic <riyajogi7@gmail.com>
2026-04-20 20:43:06 +0530

    basics of javascript

---
+++ JavaScriptBasic/script.js
@@ -0,0 +1,17 @@
+console.log("hello world!");
+
+let b = 3;
+
+var a = 5;
+var a = 10;
+
+const pi = 3.14;
+
+let x = 10;
+let y = '10';
+console.log(x==y);
+console.log(x===y);
+console.log(typeofx);
+console.log(typeofy);
+
+
