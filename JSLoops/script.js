668526dbb0e0d662140b61da31e9e0e3df64a579
Riyalogic <riyajogi7@gmail.com>
2026-04-29 23:22:36 +0530

    javascript loops

---
+++ JSLoops/script.js
@@ -0,0 +1,33 @@
+// for(var i = 0;i<=10;i++){
+//     console.log(i);
+// }
+
+// for(var j= 1;j<=10;j++)
+// {
+//     console.log(2*j); 
+// }
+
+// let a = [24,58,33,86,96,12,53,23,32,100]
+// for(let i = 0;i<10 ;i++) 
+// {
+//     console.log(a[i]);  
+// }
+
+// let k =0;
+// while(k<10){
+//     console.log(a[k]);
+//     k++;
+// }
+
+let i =1;
+while(i<=10){
+    console.log(2*i);
+    i++;
+}
+
+let j=1;
+do{
+    console.log(2*j);
+    j++;
+}
+while(j<=10)
