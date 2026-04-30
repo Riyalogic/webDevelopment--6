90958e37c3cd7f41195064e1fff4399777232bd9
Riyalogic <riyajogi7@gmail.com>
2026-04-30 17:13:13 +0530

    javascript loop task

---
+++ JSLoops/ForLoopTask/script.js
@@ -0,0 +1,42 @@
+//  Question 1
+for(var a = 1;a<=5;a++){
+    console.log(a);
+}
+    // Question 2
+for(var b= 1;b<=10;b++){
+    console.log(b); 
+}
+    // Question 3
+for(var c=10;c>=1;c--){
+        console.log(c);
+}
+    // Question 4
+for(var d = 2;d<= 10; d+= 2){
+    console.log(d);
+}
+// Question 5
+for(var e = 1;e <+9; e+=2){
+    console.log(e);
+}
+// Question 6
+for(var f= 1;f<=10;f++){
+    console.log(15*f); 
+ }
+//  question 7
+for(var g=1;g<=20; g+= 2){
+    console.log(g);
+}
+// Question 8
+for(var h=5;h<=50; h+=5){
+    console.log(h);
+}
+// Question 9
+for(var i=1;i<=10;i++){
+    console.log(i*i);
+}
+// Question 10
+for(var j=1;j<=10;j++){
+    if(j%3 == 0){
+        console.log(j);
+    }
+}
---
+++ JSLoops/WhileLoopTask/script.js
@@ -0,0 +1,79 @@
+// Question 1
+let a =1;
+while(a<=5){
+    console.log(2*a);
+    a++;
+}
+// Question 2
+let b = 5;
+while(b>=1){
+    console.log(b);
+    b++;
+}
+// Question 3
+let c = 2;
+while(c<11){
+    console.log(c);
+    c +=2;
+}
+// Question 4
+let sum =0;
+let d =10;
+while(j>=5){
+    sum += j;
+    j--
+}
+console.log("Sum =",sum);
+
+// Question 5
+let e=1;
+while(e <=5){
+    console.log(e);
+    e++;
+}
+// Question 6
+let f=1;
+while(f <=5){
+    console.log(f);
+    f++;
+}
+// Question 7
+let g = 5;
+while(g<3){
+    console.log(g);
+}
+// Question 8
+let h = 1;
+while (h<=5) {
+    console.log(h); 
+}
+// Question 9
+let i =1;
+while (i>0) {
+    console.log(i);
+}
+// Question 10
+let j= 1;
+while(j <=5){
+    console.log(j);
+    j++;
+}
+// Question 11
+let k= 1;
+while (k<=10) {
+    console.log("increment:",k);
+    k++;
+}
+let l= 10;
+while (l>=1) {
+    console.log("decrement:",l);
+    l--;
+}
+// Question 12
+let count = 0;
+let m = 1;
+while (m<=10) {
+    console.log(m);
+    m++;
+    count++;
+}
