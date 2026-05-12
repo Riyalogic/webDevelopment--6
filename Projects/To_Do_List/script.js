27b94f982b0f69a138be6f1dfeb40af6b678a452
Riyalogic <riyajogi7@gmail.com>
2026-05-12 18:59:40 +0530

    changes in To Do List

--- Projects/To_Do_List/script.js
+++ Projects/To_Do_List/script.js
@@ -1,46 +1,88 @@
-function addNewTask(){
-    const taskToAdd = document.getElementById("newTask").value;
+// function addNewTask(){
+//     const taskToAdd = document.getElementById("newTask").value;
 
-    const LI = document.createElement("li");
-    LI.classList.add("my-3");
+//     const LI = document.createElement("li");
+//     LI.classList.add("my-3");
 
-    const DIV =document.createElement("div");
-    DIV.classList.add("d-flex","align-items-center");
+//     const DIV =document.createElement("div");
+//     DIV.classList.add("d-flex","align-items-center");
 
-    const SPAN1 = document.createElement("span");
-    SPAN1.innerText = taskToAdd;
+//     const SPAN1 = document.createElement("span");
+//     SPAN1.innerText = taskToAdd;
 
-    const BUTTON = document.createElement("button");
-    BUTTON.classList.add(    
-        "btn",
-        "btn-danger",
-        "ms-4",
-        "d-flex",
-        "gap-3",
-        "align-items-center",
-        "justify-content-center"
-    );
-    BUTTON.onclick = () =>LI.remove();
+//     const BUTTON = document.createElement("button");
+//     BUTTON.classList.add(    
+//         "btn",
+//         "btn-danger",
+//         "ms-4",
+//         "d-flex",
+//         "gap-3",
+//         "align-items-center",
+//         "justify-content-center"
+//     );
+//     BUTTON.onclick = () =>LI.remove();
 
-    const I = document.createElement("i");
-    I.classList.add("bi","bi-trash");
+//     const I = document.createElement("i");
+//     I.classList.add("bi","bi-trash");
 
-    const SPAN2 = document.createElement("span");
-    SPAN2.innerText = "Delete";
+//     const SPAN2 = document.createElement("span");
+//     SPAN2.innerText = "Delete";
 
-    BUTTON.appendChild(I);
-    BUTTON.appendChild(SPAN2);
+//     BUTTON.appendChild(I);
+//     BUTTON.appendChild(SPAN2);
 
-    DIV.appendChild(SPAN1);
-    DIV.appendChild(BUTTON);
+//     DIV.appendChild(SPAN1);
+//     DIV.appendChild(BUTTON);
 
-    LI.appendChild(DIV);
+//     LI.appendChild(DIV);
 
-    document.getElementById("taskDiv").classList.remove("d-none");
+//     document.getElementById("taskDiv").classList.remove("d-none");
 
-    document.getElementById("taskList").appendChild(LI);
+//     document.getElementById("taskList").appendChild(LI);
 
-    document.getElementById("newTask").value = "";
+//     document.getElementById("newTask").value = "";
+// }
+function addNewTask() {
+  const taskToAdd = document.getElementById("newTask").value;
 
-    
+  const LI = document.createElement("li");
+  LI.classList.add("my-3");
+
+  const DIV = document.createElement("div");
+  DIV.classList.add("d-flex", "align-items-center");
+
+  const SPAN1 = document.createElement("span");
+  SPAN1.innerText = taskToAdd;
+
+  const BUTTON = document.createElement("button");
+  BUTTON.classList.add(
+    "btn",
+    "btn-danger",
+    "ms-4",
+    "d-flex",
+    "gap-3",
+    "align-items-center",
+    "justify-content-center",
+  );
+  BUTTON.onclick = () => LI.remove();
+
+  const I = document.createElement("i");
+  I.classList.add("bi", "bi-trash");
+
+  const SPAN2 = document.createElement("span");
+  SPAN2.innerText = "Delete";
+
+  BUTTON.appendChild(I);
+  BUTTON.appendChild(SPAN2);
+
+  DIV.appendChild(SPAN1);
+  DIV.appendChild(BUTTON);
+
+  LI.appendChild(DIV);
+
+  document.getElementById("taskDiv").classList.remove("d-none");
+
+  document.getElementById("taskList").appendChild(LI);
+
+  document.getElementById("newTask").value = "";
 }
