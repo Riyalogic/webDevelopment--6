b89a926b51ae86db6b4161340ddf56d08b39fbfe
Riyalogic <riyajogi7@gmail.com>
2026-05-24 10:50:16 +0530

    Joke Task

---
+++ Projects/JokeApp/script.js
@@ -0,0 +1,26 @@
+async function GetNewJoke(){
+    // const API_URL = "https://v2.jokeapi.dev/joke/Programming";
+
+    const API_URL = "https://official-joke-api.appspot.com/jokes/random";
+
+    const response = await fetch(API_URL);
+
+    // console.log(response);
+
+    const data = await response.json();
+    
+    // console.log(data);
+
+    // if (data.type === "single") {
+    //     document.getElementById("setup").innerText = data.joke;
+    //     document.getElementById("delivery").innerText = "";
+    // }
+    // else{
+    //     document.getElementById("setup").innerText = data.setup;
+    //     document.getElementById("delivery").innerText = data.delivery; 
+    // }
+
+    document.getElementById("setup").innerText = data.joke || data.setup;
+    document.getElementById("punchline").innerText = data.punchline || "";
+    
+}
