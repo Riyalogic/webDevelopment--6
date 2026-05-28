7b351c6f156fed6e896e765d29f5398e814ab878
Riyalogic <riyajogi7@gmail.com>
2026-05-28 23:19:52 +0530

    changes in currency calculator

--- Projects/Currency_Converter/script.js
+++ Projects/Currency_Converter/script.js
@@ -1,14 +1,4 @@
-//   const countryList [
-//    {
-//     countryCode = "INR",
-//     countryName = "India",
-//     currCode = "INR"
-//   },
-//   {
-    
-//   }
-//   ];
-// const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
+// const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currencyCode.toLowerCase()}.json";
 
 // const dropdowns = document.querySelectorAll(".dropdown select");
 // const btn = document.querySelector("form button");
@@ -72,3 +62,54 @@
 // window.addEventListener("load", () => {
 //   updateExchangeRate();
 // });
+
+
+const country = [
+  {
+    countryCode: "IN",
+    countryName: "India",
+    currencyCode: "INR",
+  },
+  {
+    countryCode: "US",
+    countryName: "USA",
+    currencyCode: "USD",
+  },
+];
+
+function loadSelectOption() {
+  const Country1 = document.getElementById("country1");
+
+  country.forEach((country) => {
+    const OP = document.createElement("option");
+
+    OP.value = country.countryCode + "_" + country.currencyCode;
+
+    OP.innerText = country.countryName;
+
+    Country1.appendChild(OP);
+  });
+}
+
+loadSelectOption();
+
+async function FetchFlag1() {
+  const code = document.getElementById("country1").value;
+  console.log(code);
+
+  const countryCode = code.split("_")[0];
+  const currencyCode = code.split("_")[1];
+
+  //   console.log(countryCode, currencyCode);
+
+  document.getElementById("country1Flag").src =
+    `https://flagsapi.com/${countryCode}/flat/64.png`;
+
+  const response = await fetch(
+    `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currencyCode.toLowerCase()}.json`,
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+}
