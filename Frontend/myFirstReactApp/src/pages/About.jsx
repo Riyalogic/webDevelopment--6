25f45f753ad4765994adda5956a27a3249ec062e
Riyalogic <riyajogi7@gmail.com>
2026-06-03 20:13:09 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/myFirstReactApp/src/pages/About.jsx
+++ Frontend/myFirstReactApp/src/pages/About.jsx
@@ -1,8 +1,10 @@
+import { FaWifi } from "react-icons/fa";
+import roseImage from"./assets/src/pages/rose.jpg"
 function About() {
   return (
     <>
       <div className="container d-flex justify-content-center align-items-center">
-        <span className="text-primary fs-2">About</span>
+        <span className="text-primary fs-2">< FaWifi  />About</span>
       </div>
     </>
   );
