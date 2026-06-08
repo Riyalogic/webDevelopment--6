8061b621a9a5d6747def484ec3db3cde296a6e03
Riyalogic <riyajogi7@gmail.com>
2026-06-08 20:39:06 +0530

    fix: correct image source syntax in App component

--- Frontend/cravingReactApp/src/App.jsx
+++ Frontend/cravingReactApp/src/App.jsx
@@ -1,4 +1,4 @@
-
+import logo from "./assets/cravinglogo.png"
 
 const App = () => {
   return (
@@ -7,7 +7,7 @@ const App = () => {
       <button className="bg-blue-400 p-3 border border-blue-800">submit</button> */}
 
       <div className=" p-3 bg-orange-600 text-white flex gap-5 ">
-        <img src="{logo}" alt="" />
+        <img src={logo} alt="bg image" />
         <div>
           <button className=" hover:bg-orange-700 text-white justify-end items-end rounded">Login</button>
           <button className=" hover:bg-orange-700 text-white justify-end items-end rounded">Register</button>
