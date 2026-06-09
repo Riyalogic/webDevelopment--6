f23e9e88499b524c440443c7f5729b9852fa3b05
Riyalogic <riyajogi7@gmail.com>
2026-06-09 20:43:09 +0530

    Refactor code structure for improved readability and maintainability

--- Frontend/cravingReactApp/src/App.jsx
+++ Frontend/cravingReactApp/src/App.jsx
@@ -6,11 +6,13 @@ const App = () => {
       {/* <div className=" bg-green-200 text-amber-800 p-3">App</div>
       <button className="bg-blue-400 p-3 border border-blue-800">submit</button> */}
 
-      <div className=" p-3 bg-orange-600 text-white flex gap-5 ">
+      <div className=" p-3 bg-orange-600 flex text-white justify-between items-end">
+        <div className=" w-20">
         <img src={logo} alt="bg image" />
+        </div>
         <div>
-          <button className=" hover:bg-orange-700 text-white justify-end items-end rounded">Login</button>
-          <button className=" hover:bg-orange-700 text-white justify-end items-end rounded">Register</button>
+          <button className=" hover:focus-within:outline-2 focus-within:outline-amber-50  text-white  gap-2 fs-5 px-4 p-2 rounded ">Login</button>
+          <button className=" bg-amber-50 hover:bg-orange-500  text-orange-500  rounded fs-5 px-4 p-2">Register</button>
         </div>
 
       </div>
