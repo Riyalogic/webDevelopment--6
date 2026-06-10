ef4b32de54ae11999a9f2f596a26ec0d883deeaf
Riyalogic <riyajogi7@gmail.com>
2026-06-10 20:49:24 +0530

    feat: initialize project structure with React, Vite, and Tailwind CSS
    - Added main entry point (main.jsx) with React StrictMode
    - Created basic components: Header, Footer, About, Contact, Home, Products
    - Included SVG assets for React and Vite logos
    - Set up Tailwind CSS in index.css
    - Configured Vite with React and Tailwind CSS plugins

--- Frontend/cravingReactApp/src/App.jsx
+++ Frontend/cravingReactApp/src/App.jsx
@@ -10,13 +10,11 @@ const App = () => {
         <div className=" w-20">
         <img src={logo} alt="bg image" />
         </div>
-        <div className=" bg-[url('/bgImage.jpg')] bg-cover h-screen w-full justify-center items-center">
-
-        </div>
         <div>
           <button className=" hover:focus-within:outline-2 focus-within:outline-amber-50  text-white  gap-2 fs-5 px-4 p-2 rounded ">Login</button>
           <button className=" bg-amber-50 hover:bg-orange-500  text-orange-500  rounded fs-5 px-4 p-2">Register</button>
         </div>
+         <div className=" bg-[url('/bgImage.jpg')] bg-cover h-screen w-full justify-center items-center"></div>
 
       </div>
     </>
---
+++ Projects/FakeStore/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import { Contact } from "./pages/Contact"
+import { BrowserRouter} from "react-router-dom"
+
+const App = () => {
+  return(
+    <>
+      <BrowserRouter>
+      <header />
+      <footer />
+
+      <Routes>
+  
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
