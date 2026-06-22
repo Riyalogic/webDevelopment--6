3b953b8afc64a00e2d62761aca87ddf0e4ba4aa5
Riya Jogi <riyajogi7@gmail.com>
2026-06-22 20:23:19 +0530

    feat: implement authentication and public routes; add controllers for login, logout, register, and contact

--- Backened/FirstBackened/index.js
+++ Backened/FirstBackened/index.js
@@ -2,17 +2,39 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import AuthRouter from "./src/routers/auth.route.js";
+import PublicRouter from "./src/routers/public.route.js";
 
 const app = express();
 
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+
+//Default API
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
   res.json({ message: "Welcome to my first backened Project" });
 });
 
-app.post("/login", (req, res) => {
-  res.json({ message: "Login successfull" });
-});
+// app.post("/login", (req, res) => {
+//   res.json({ message: "Login successfull" });
+// });
+
+// app.post("/logout", (req, res) => {
+//   res.json({ message: "Logout successfull" });
+// });
+
+// app.post("/register", (req, res) => {
+//   res.json({ message: "register successfull" });
+// });
+
+// app.put("/update", (req, res) => {
+//   res.json({ message: "update successfull" });
+// });
+
+// app.delete("/delete", (req, res) => {
+//   res.json({ message: "delete successfull" });
+// });
 
 const port = process.env.Port || 5000;
 
